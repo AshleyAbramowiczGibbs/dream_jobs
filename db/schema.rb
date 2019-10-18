366deef4c0454ff93a785eec8eb93613a6ad1d8a
Ashley Abramowicz <ashleyabramo2@gmail.com>
2019-10-18 16:26:44 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180331012601) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.text "description"
